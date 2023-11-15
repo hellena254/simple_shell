87f80cd3303e0db10b1c27f910b54b67cb518b3c
martine-K <kungumartine06@gmail.com>
2023-11-14 22:21:56 -0800

    builtin environment

--- shell.h
+++ shell.h
@@ -29,6 +29,48 @@ extern char **environ;
 char *name;
 int hist;
 
+typedef struct list_s
+{
+	char *dir;
+	struct list_s *next;
+} list_t;
+
+typedef struct builtin_s
+{
+	char *name;
+	int (*f)(char **argv, char **front);
+} builtin_t;
+
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char **_strtok(char *line, char *delim);
+char *get_location(char *command);
+list_t *get_path_dir(char *path);
+int execute(char **args, char **front);
+void free_list(list_t *head);
+char *_itoa(int num);
+
+/* Input Helpers functions*/
+void handle_line(char **line, ssize_t read);
+void variable_replacement(char **args, int *exe_ret);
+char *get_args(char *line, int *exe_ret);
+int call_args(char **args, char **front, int *exe_ret);
+int run_args(char **args, char **front, int *exe_ret);
+int handle_args(int *exe_ret);
+int check_args(char **args);
+void free_args(char **args, char **front);
+char **replace_aliases(char **args);
+
+/* String functions */
+int _strlen(const char *s);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+char *_strcpy(char *dest, const char *src);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
+
 /* Builtins environment*/
 int (*get_builtin(char *command))(char **args, char **front);
 int shellby_exit(char **args, char **front);
@@ -49,4 +91,5 @@ char *error_2_syntax(char **args);
 char *error_126(char **args);
 char *error_127(char **args);
 
+int proc_file_commands(char *file_path, int *exe_ret);
 #endif
